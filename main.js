10dfd38c6c2163d8c95cd5685fb527e399fea2ba
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-07-07 14:02:33 -0400

    Update to problem

--- main.js
+++ main.js
@@ -24,17 +24,21 @@ const getRandomNumber = function(max) {
 // let button_d6 = document.querySelector("#d6-button");
 let img_d6 = document.querySelector("#d6-roll");
 let mean_d6 = document.querySelector("#d6-rolls-mean");
+let median_d6 = document.querySelector("#d6-rolls-median");
 
 let img_d_d6_roll = document.querySelector("#double-d6-buttons");
 let img_d_d6_1 = document.querySelector("#double-d6-roll-1");
 let img_d_d6_2 = document.querySelector("#double-d6-roll-2");
 let mean_d_d6 = document.querySelector("#double-d6-rolls-mean");
+let median_d_d6 = document.querySelector("#double-d6-rolls-median");
 
 let img_d12 = document.querySelector("#d12-roll");
 let mean_d12 = document.querySelector("#d12-rolls-mean");
+let median_d12 = document.querySelector("#d12-rolls-median");
 
 let img_d20 = document.querySelector("#d20-roll");
 let mean_d20 = document.querySelector("#d20-rolls-mean");
+let median_d20 = document.querySelector("#d20-rolls-median");
 
 let reset_button = document.querySelector("#reset-button");
 
@@ -42,19 +46,46 @@ let reset_button = document.querySelector("#reset-button");
  * EVENT LISTENERS *
  *******************/
 img_d6.addEventListener('click', d6RollFunc);
+img_d6.addEventListener('click', function(){
+    meanD6(sixes);
+});
+img_d6.addEventListener('click', function(){
+    medianD6(sixes);
+})
 
-img_d_d6_roll.addEventListener('click', dd6RollFunc)
+img_d_d6_roll.addEventListener('click', dd6RollFunc);
+img_d_d6_roll.addEventListener('click', function(){
+    meanDD6(doubleSixes);
+});
+img_d_d6_roll.addEventListener('click', function(){
+    medianDD6(doubleSixes);
+})
 
 img_d12.addEventListener('click', d12RollFunc);
+img_d12.addEventListener('click', function(){
+    meanD12(twelves);
+});
+img_d12.addEventListener('click', function(){
+    medianD12(twelves);
+})
 
 img_d20.addEventListener('click', d20RollFunc);
+img_d20.addEventListener('click', function(){
+    meanD20(twenties);
+});
+img_d20.addEventListener('click', function(){
+    medianD20(twenties);
+})
+
 
 reset_button.addEventListener('click', resetFunction);
 
 /******************
  * RESET FUNCTION *
  ******************/
-resetFunction();
+ resetFunction();
+
+
 function resetFunction(){
     sixes = [];
     doubleSixes = [];
@@ -63,16 +94,20 @@ function resetFunction(){
 
     img_d6.src = "images/start/d6.png";
     mean_d6.innerText = "N/A";
+    median_d6.innerText = "N/A";
 
     img_d_d6_1.src = "images/start/d6.png";
     img_d_d6_2.src = "images/start/d6.png";
     mean_d_d6.innerText = "N/A";
+    median_d_d6.innerText = "N/A";
 
     img_d12.src = "images/start/d12.jpeg";
     mean_d12.innerText = "N/A";
+    median_d12.innerText = "N/A";
 
     img_d20.src = "images/start/d20.jpg";
     mean_d20.innerText = "N/A";
+    median_d20.innerText = "N/A";
 
 }
 
@@ -309,3 +344,47 @@ function d20RollFunc(){
 /****************
  * MATH SECTION *
  ****************/
+function meanD6(sixes){
+    let sum = 0;
+    let mean = 0;
+    let len = sixes.length;
+    for (let i of sixes){
+        sum += i;
+    }
+    mean = Math.ceil(sum / len);
+    mean_d6.innerText = mean;
+}
+
+function meanDD6(doubleSixes){
+    let sum = 0;
+    let mean = 0;
+    let len = doubleSixes.length;
+    for (let i of doubleSixes){
+        sum += i;
+    }
+    mean = Math.ceil(sum / len);
+    mean_d_d6.innerText = mean;
+}
+
+function meanD12(twelves){
+    let sum = 0;
+    let mean = 0;
+    let len = twelves.length;
+    for (let i of twelves){
+        sum += i;
+    }
+    mean = Math.ceil(sum / len);
+    mean_d12.innerText = mean;
+}
+
+function meanD20(twenties){
+    let sum = 0;
+    let mean = 0;
+    let len = twenties.length;
+    for (let i of twenties){
+        sum += i;
+    }
+    mean = Math.ceil(sum / len);
+    mean_d20.innerText = mean;
+}
+
---
+++ workout.js
@@ -0,0 +1,4 @@
+function medianD6(sixes){
+    let res = 0;
+    sixes.sorted
+}
