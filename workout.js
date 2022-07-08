c5b411a5a71f59278dc39327d7d145046c12d2f0
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-07-08 11:21:40 -0400

    Update workout.js

--- workout.js
+++ workout.js
@@ -1,4 +1,14 @@
-function medianD6(sixes){
-    let res = 0;
-    sixes.sorted
+let sixes = [1,2,3,4,5,6];
+
+function medianD6(arr){
+    let sorted = arr.sort(function(a,b){
+        return a - b;
+    })
+    let mid = sorted.length / 2;
+    if (sorted.length % 2 == 0){
+        return (sorted[mid - 1] + sorted[mid]) / 2;
+    }
+    return sorted[middle];
 }
+
+console.log(medianD6(sixes));
